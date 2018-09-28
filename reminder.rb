91391c68a107f94f8bb5de32a197ead771f85573
EC2 Default User <ec2-user@ip-172-31-7-192.us-east-2.compute.internal>
2018-09-28 13:28:22 +0000

    Austen's start

---
+++ reminder.rb
@@ -0,0 +1,43 @@
+@tasks = [] # empty array to be added to 
+
+def view_list # method to load the list for the user
+    
+end
+
+
+def add_list # method to add an item to the list
+puts "Please enter a task below:"
+task = gets.chomp
+  while !task.empty? do  # loop to get another item on the list
+    @tasks << task # adds teh user input to the empty tasks array
+    puts "You have added #{task} to your list"
+    puts ""
+    puts "Now add another task to the list"
+    task = gets.chomp # second input
+  end
+end
+
+def delete_list # method to delete an item from the list
+
+end
+
+
+
+puts "Choose what you would like to do:" # user menu; puts all the options
+puts "a) view list"
+puts "b) add to list"
+puts "c) delete from list"
+
+option =  gets.chomp # takes the selection from the menu
+
+if option == "a" # what to do for each menu input
+  puts "You have selected: view list" 
+  view_list
+  elsif option == "b"
+    puts "You have selected: add to list"
+    add_list
+  elsif option == "c"
+    puts "You have selected: delete from list"
+    delete_list
+end
+
